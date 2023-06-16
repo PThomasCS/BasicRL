d63f48519e5d49e2637f5ee07025b11f06102ad2
Philip Thomas <PThomasCS@gmail.com>
2023-06-16 13:07:10 -0400

    Included computations of importance weights and printing them to a file.

--- BasicRL/June16_2023.cpp
+++ BasicRL/June16_2023.cpp
@@ -14,10 +14,13 @@ void sandboxJune16_2023()
 {
 	mt19937_64 generator;
 
-	double pMean = 0, qMean = 3, pSigmaSquared = 1, qSigmaSquared = 1;
+	double pMean = 0, qMean = 1, pSigmaSquared = 1, qSigmaSquared = 1;
 	normal_distribution<double> p(pMean, pSigmaSquared), q(qMean, qSigmaSquared);
 
 	int n = 500000;
+
+	// Vector for storing the importance weights
+	vector<double> importanceWeights(n);
 
 	// Draw samples
 	VectorXd samples(n);
@@ -28,7 +31,9 @@ void sandboxJune16_2023()
 	double ISEstimate = 0;
 	for (int i = 0; i < n; i++)
 	{
-		ISEstimate += normalPDF(samples[i], pMean, pSigmaSquared) / normalPDF(samples[i], qMean, qSigmaSquared) * samples[i];
+		double importanceWeight = normalPDF(samples[i], pMean, pSigmaSquared) / normalPDF(samples[i], qMean, qSigmaSquared);
+		importanceWeights[i] = importanceWeight;
+		ISEstimate += importanceWeight * samples[i];
 	}
 	ISEstimate /= (double)n;	// (double) is making sure that we treat n as a floating point number, not an integer
 
@@ -43,5 +48,18 @@ void sandboxJune16_2023()
 	cout << "pMean \t\t\t\t\t\t= " << pMean << endl;
 	cout << "Estimate of pMean by sampling from p \t\t= " << meanFromPSamples << endl;
 	cout << "IS estimate of pMean when sampling from q \t= " << ISEstimate << endl;
-	
+
+	// By the time we get here, we have loaded importanceWeights. How should we visualize it? How should we compute properties of it?
+	// First lets print the mean
+	double averageImportanceWeight = 0;
+	for (int i = 0; i < n; i++)
+		averageImportanceWeight += importanceWeights[i];
+	averageImportanceWeight /= (double)n;
+	cout << "The sample mean of the importance weights was " << averageImportanceWeight << endl;
+
+	// Print the importance weights to a file
+	ofstream out("out/June16_2023_importanceWeights.txt");
+	for (int i = 0; i < n; i++)
+		out << importanceWeights[i] << endl;
+	out.close();	// You don't have to do this, it will close when the "out" variable is cleaned up, which happens when this function ends.
 }
--- BasicRL/stdafx.hpp
+++ BasicRL/stdafx.hpp
@@ -18,6 +18,7 @@
 #include <vector>					// For vector data structures - not for linear algebra
 #include <string>					// For using strings as opposed to char* (the old C++ way).
 #include <math.h>					// For some math terms like M_PI
+#include <fstream>					// For reading from and printing to files, just like with cout/cin.
 
 ////////////////////////////////////////////////////////////////
 // Additional libraries
