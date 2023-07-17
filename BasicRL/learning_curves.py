9078ea7ea896a8e5d17a448e911f7a67c3c6c860
Alexandra Burushkina <alexandra.burushkin@gmail.com>
2023-07-17 12:17:09 -0400

    Updated Python plot.

--- BasicRL/learning_curves.py
+++ BasicRL/learning_curves.py
@@ -2,58 +2,68 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
  
 
 def plot_graph():
+    # df = pd.read_csv("out/results.csv")
+    # plt.errorbar(df['Episode'], df['Average Discounted Return'], yerr=df['Standard Error'], ecolor=['grey'])
+    # plt.xlabel('Episode')
+    # plt.ylabel('Average Discounted Return')
+    # plt.savefig("out/plot.jpg", format='jpeg')
+    # plt.show()
     df = pd.read_csv("out/results.csv")
-    plt.errorbar(df['Episode'], df['Average Discounted Return'], yerr=df['Standard Error'], ecolor=['grey'])
+    sns.set(style='whitegrid')
+    sns.lineplot(data=df, x='Episode', y='Average Discounted Return')
+    plt.fill_between(df['Episode'], df['Average Discounted Return'] - df['Standard Error'], df['Average Discounted Return'] + df['Standard Error'], color='tab:orange', alpha=0.3)
     plt.xlabel('Episode')
     plt.ylabel('Average Discounted Return')
     plt.savefig("out/plot.jpg", format='jpeg')
     plt.show()
 
 
-# TO-DO: add new graphs (e.g., cumulative reward, etc.)
-
-# Plot learning curve showing cumulative actions taken so far on the x-axis and number of episodes on the y-axis
-# If the agent is learning, the graph should have an increasing slope
-
-def plot():
-    x = []
-
-    # x-axis data showing cumulative actions taken
-    with open('cmake-build-debug/actions.txt', 'r') as f:
-        for line in f:
-            data = line.strip()  # Strip whitespace
-            x.append(float(data))  # Convert to float
-
-    # y-axis data showing number of episodes
-    y = list(range(1, len(x) + 1))
-
-    plt.plot(x, y)
-    plt.ticklabel_format(style='plain')
-    plt.xlabel('# Actions Taken')
-    plt.ylabel('# Episodes')
-    plt.show()
-
-
-def plot_mc():
-    y = []
-
-    # y-axis data showing the number of steps needed by the agent to reach the goal state
-    with open('cmake-build-debug/returns.txt', 'r') as f:
-        for line in f:
-            data = line.strip()  # Strip whitespace
-            y.append(float(data) * (-1))  # Convert to float
-
-    # x-axis data showing number of episodes
-    x = list(range(1, len(y) + 1))
-
-    plt.plot(x, y)
-    plt.ticklabel_format(style='plain')
-    plt.xlabel('# Episodes')
-    plt.ylabel('# Steps to reach goal')
-    plt.show()
+#
+# # TO-DO: add new graphs (e.g., cumulative reward, etc.)
+#
+# # Plot learning curve showing cumulative actions taken so far on the x-axis and number of episodes on the y-axis
+# # If the agent is learning, the graph should have an increasing slope
+#
+# def plot():
+#     x = []
+#
+#     # x-axis data showing cumulative actions taken
+#     with open('cmake-build-debug/actions.txt', 'r') as f:
+#         for line in f:
+#             data = line.strip()  # Strip whitespace
+#             x.append(float(data))  # Convert to float
+#
+#     # y-axis data showing number of episodes
+#     y = list(range(1, len(x) + 1))
+#
+#     plt.plot(x, y)
+#     plt.ticklabel_format(style='plain')
+#     plt.xlabel('# Actions Taken')
+#     plt.ylabel('# Episodes')
+#     plt.show()
+#
+#
+# def plot_mc():
+#     y = []
+#
+#     # y-axis data showing the number of steps needed by the agent to reach the goal state
+#     with open('cmake-build-debug/returns.txt', 'r') as f:
+#         for line in f:
+#             data = line.strip()  # Strip whitespace
+#             y.append(float(data) * (-1))  # Convert to float
+#
+#     # x-axis data showing number of episodes
+#     x = list(range(1, len(y) + 1))
+#
+#     plt.plot(x, y)
+#     plt.ticklabel_format(style='plain')
+#     plt.xlabel('# Episodes')
+#     plt.ylabel('# Steps to reach goal')
+#     plt.show()
 
 
 if __name__ == "__main__":
